dce1ed614f4abb1b5b37623c7839d2f602e90084
kylecsunaccount <kyle.drainudel.96@my.csun.edu>
2021-02-22 23:29:57 -0800

    added java for selenium and broke the webpage for the test

---
+++ selenium/htmlUnitTest.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.junit.Assert;
+
+public class htmlUnitTest {
+
+	public static void main(String[] args) {
+
+		// Creating a new instance of the HTML unit driver
+		WebDriver driver = new HtmlUnitDriver(true); //enable JavaScript
+
+		// Navigate to local Jekyll site
+		driver.get("http://192.168.0.241:4000");
+
+		// This code will test and print the page title
+		String pageTitle = driver.getTitle();
+		Assert.assertNotEquals(pageTitle, "");
+		Assert.assertFalse(pageTitle.matches("^\\s*$")); // uses regexp
+		System.out.println("Page title is: " + pageTitle);
+
+		// This code will check the HTML source for Jekyll 404 errors
+		String pageSource = driver.getPageSource();
+		Assert.assertFalse(pageSource.contains("404.html"));
+		Assert.assertFalse(pageSource.contains("requested page could not be found"));
+
+		// Create a test that is specific to YOUR Jekyll site here -->
+		String actualEmail = driver.findElement(By.className("u-email")).getText();
+		String expectedEmail = "kyle.drainudel.96@my.csun.edu";
+		Assert.assertEquals(actualEmail, expectedEmail);
+		System.out.println("The email address is: " + actualEmail);
+		// <-- End of your test code
+
+		driver.quit();
+	}
+}
